e31d199ba9cd1ee037ce2d12dbdb40f5f97061b7
Raphael Spencer <raphael@headway.io>
2019-02-06 18:08:29 -0600

    Removed unnecessary BuildingEscape directory.

---
+++ Source/BuildingEscape/Grabber.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "Grabber.generated.h"
+
+struct GrabberLine {
+	FVector Start;
+	FVector End;
+};
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+private:
+	float Reach = 100.f;
+	APlayerController* player = nullptr;
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UInputComponent* InputComponent = nullptr;
+	FVector GrabberLineBegin;
+	FVector GrabberLineEnd;
+
+	// Ray-cast and grab what's in reach.
+	void Grab();
+	void Release();
+	GrabberLine GetGrabberLine();
+
+	const FHitResult GetFirstPhysicsBodyInReach();
+};
